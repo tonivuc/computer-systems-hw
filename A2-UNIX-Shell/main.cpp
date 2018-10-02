7697ea50dbab8fe791a28b33cec4c674a4c1c7ae
Toni Vucic <tonivuc@gmail.com>
2018-10-02 15:36:56 -0500

    Added stringTokenizer and credits. yet to put to use

--- A2-UNIX-Shell/main.cpp
+++ A2-UNIX-Shell/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <vector>
 
 using namespace std;
 
@@ -10,11 +11,13 @@ int main() {
     string input;
     int status;
     bool cont = true;
-    //TODO: Continually prompt for textual user input on a command line
+
     cout << "Enter command: ";
     while ( getline( cin, input )) {
 
         cout << input;
+
+
 
 
         int pid = fork();
@@ -27,4 +30,31 @@ int main() {
         }
         cont = false;
     }
+}
+
+/***************************************************************************************
+*    Title: String tokenizer in C++ w. delimiter characters
+*    Author: linello (Stackoverflow username)
+*    Date: 10.02.2018
+*    Code version: 02.02.2014 14:27
+*    Availability: https://stackoverflow.com/questions/9823263/string-tokenization-in-c-including-delimiter-characters
+*
+***************************************************************************************/
+vector<string> tokenizeString(const string& str, const string& delimiters)
+{
+    vector<string> tokens;
+    // Skip delimiters at beginning.
+    string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+    // Find first "non-delimiter".
+    string::size_type pos = str.find_first_of(delimiters, lastPos);
+
+    while (string::npos != pos || string::npos != lastPos)
+    {  // Found a token, add it to the vector.
+        tokens.push_back(str.substr(lastPos, pos - lastPos));
+        // Skip delimiters.  Note the "not_of"
+        lastPos = str.find_first_not_of(delimiters, pos);
+        // Find next "non-delimiter"
+        pos = str.find_first_of(delimiters, lastPos);
+    }
+    return tokens;
 }
