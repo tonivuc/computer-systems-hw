5f64b90f23a55e7781b730b0e7caa8c93b71f769
Toni Vucic <tonivuc@gmail.com>
2018-10-22 11:53:46 -0500

    Added A4 source files

--- A4-Synchronization/BoundedBuffer.cpp
+++ A4-Synchronization/BoundedBuffer.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 BoundedBuffer::BoundedBuffer(int _cap) {
-	
+
 }
 
 BoundedBuffer::~BoundedBuffer() {
@@ -20,7 +20,18 @@ void BoundedBuffer::push(string str) {
 	Is this function thread-safe??? Does this automatically wait for the pop() to make room 
 	when the buffer if full to capacity???
 	*/
-	q.push (str);
+
+
+	//This one mutex is for the entire boundedbuffer? To avoid race conditions?
+    pthread_mutex_lock(&mtx);
+    while (buffer.size () == 0){ //The correct solution for this is of course to check if the wakeup was actually legit before proceding. (Because pthread wait is sometimes woken even if it wasn't signaled to be woken)
+        pthread_cond_wait(&cond, &mtx);
+        //V--?
+    }
+    // now consume
+    q.push (str);
+    pthread_cond_signal (&cond2); // send signal to Producer(s) that we have consumed. (V++)
+    pthread_mutex_unlock(&mtx);
 }
 
 string BoundedBuffer::pop() {
