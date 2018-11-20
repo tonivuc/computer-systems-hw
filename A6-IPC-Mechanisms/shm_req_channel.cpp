1e1fb466995560cd2e28a2b98f7c9a33b1bf288d
Toni Vucic <tonivuc@gmail.com>
2018-11-20 13:14:35 -0600

    Added the starter codeto shm_req_channel

--- A6-IPC-Mechanisms/shm_req_channel.cpp
+++ A6-IPC-Mechanisms/shm_req_channel.cpp
@@ -2,4 +2,68 @@
 // Created by toni on 11/16/18.
 //
 
+#include <cstring>
 #include "shm_req_channel.h"
+
+SHMRequestChannel::SHMRequestChannel(const std::string _name, const Side _side) {
+    key_t key;
+    int shmid;
+    string filename = _name;
+
+    std::ofstream file {filename.c_str()}; //Use a vector to delete these later?
+    filenames.push_back(filename);
+
+    /* make the key: */
+    if ((key = ftok(filename.c_str(), 200)) == -1) {
+        perror("ftok");
+        exit(1);
+    }
+
+    /* Create the segment: */
+    if ((shmid = shmget(key, SHM_SIZE, 0644 | IPC_CREAT)) == -1) {
+        perror("shmget");
+        exit(1);
+    }
+
+    /* attach to the segment to get a pointer to it: */
+    data = (char*)shmat(shmid, 0, 0);
+    if (*data == -1) {
+        perror("shmat");
+        exit(1);
+    }
+}
+
+
+int SHMRequestChannel::cwrite(string msg) {
+    /* read or modify the segment, based on the command line: */
+    printf("writing to segment: \"%s\"\n", msg.c_str());
+    strncpy(data, msg.c_str(), SHM_SIZE);
+    return 0;
+}
+
+string SHMRequestChannel::cread() {
+
+    /* read from the shared-memory segment: */
+    printf("***********segment contains: \"%s\"\n", data);
+
+    string s = data;
+    return s;
+}
+
+
+
+SHMRequestChannel::~SHMRequestChannel() {
+    /* detach from the segment: */
+    if (shmdt(data) == -1) {
+        perror("shmdt");
+        exit(1);
+    }
+    //Remove files created
+    for (int i = 0; i < filenames.size(); i++) {
+        remove(filenames.at(i).c_str());
+    }
+}
+
+string SHMRequestChannel::getServerOrClient() {
+    return (my_side == SHMRequestChannel::SERVER_SIDE) ? "SERVER" : "CLIENT";
+}
--- A6-IPC-Mechanisms/shm_req_channel.h
+++ A6-IPC-Mechanisms/shm_req_channel.h
@@ -5,10 +5,40 @@
 #ifndef A6_IPC_MECHANISMS_SHMREQUESTCHANNEL_H
 #define A6_IPC_MECHANISMS_SHMREQUESTCHANNEL_H
 #import "requestchannel.h"
+#include <sys/shm.h>
+#include <vector>
+
+#define SHM_SIZE 1024  /* make it a 1K shared memory segment */
 
 
 class SHMRequestChannel : public RequestChannel {
+    
+private:
+    Side     my_side;
+    char *data;
+    vector<string> filenames;
 
+
+public:
+
+    SHMRequestChannel ( const string _name , const Side _side);
+    SHMRequestChannel();
+
+    string cread();
+    /* Blocking read of data from the channel. Returns a string of
+    characters read from the channel. Returns NULL if read failed. */
+
+    int cwrite(string msg);
+
+    int createQueue(string mqFileName, Mode mode);
+    string mq_name(Mode _mode);
+
+    string getServerOrClient();
+
+    ~SHMRequestChannel();
+    /* Write the data to the channel. The function returns
+    the number of characters written to the channel. */
+};
 };
 
 
